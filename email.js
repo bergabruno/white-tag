df5f4280c5636bd628dbedcfe42b4b207056e84a
bergabruno <brunoberga77@gmail.com>
2022-10-16 10:09:13 -0300

    adicionando cleartable

--- email.js
+++ email.js
@@ -12,17 +12,20 @@ function criaLinha(email){
     tdStatus = document.createElement("td")
     tdDestinatario = document.createElement("td")
     tdTitulo = document.createElement("td")
+    tdTexto = document.createElement("td")
     tdData = document.createElement("td")
 
     tdStatus.innerHTML = email.statusEmail;
     tdDestinatario.innerHTML = email.emailTo;
     tdTitulo.innerHTML = email.subject;
+    tdTexto.innerHTML = email.text;
     tdData.innerHTML = email.sendDateEmail;
 
 
     linha.appendChild(tdStatus)
     linha.appendChild(tdDestinatario)
     linha.appendChild(tdTitulo)
+    linha.appendChild(tdTexto)
     linha.appendChild(tdData)
 
     return linha;
@@ -33,11 +36,27 @@ function main(){
     let data = fazGet("http://localhost:8082/api/v1/email"); 
     let emails = JSON.parse(data);  
 
-    let tabela = document.getElementById("tabelaEmail");
+    const table = document.querySelector("#tabelaEmail tbody")
     emails.forEach(element => {
         let linha = criaLinha(element);
-        tabela.appendChild(linha);
+        table.appendChild(linha);
     });
-    //para cada user criar uma linha e add na tabela
+}
 
+function filtroPorData(){
+    console.log("estou aqui");
+    let dia = document.getElementById("date");
+    
+    const table = document.querySelector("#tabelaEmail tbody")
+    while(table.childElementCount > 0){
+        table.deleteRow(0);
+    }
+    
+    let data = fazGet(`http://localhost:8082/api/v1/email/${dia.value}`); 
+    let emails = JSON.parse(data);
+
+    emails.forEach(element => {
+        let linha = criaLinha(element);
+        table.appendChild(linha);
+    });
 }
